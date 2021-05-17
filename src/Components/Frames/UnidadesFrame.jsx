19aadea02528ceb5691166736a40e5146fd6a135
Zaid <zaid_lorea@yahoo.com.mx>
2021-05-16 20:02:32 -0500

    New unidad window list and edit functionality added

--- src/Components/Frames/UnidadesFrame.jsx
+++ src/Components/Frames/UnidadesFrame.jsx
@@ -1,45 +1,102 @@
 import React, {useState, useEffect} from 'react'
 import Nuevo from '../../Assets/Nuevo.svg'
+import SearchBar from '../SearchBar'
+
+const {ipcRenderer} = window.require('electron')
 
 function UnidadesFrame (props){
     useEffect(()=>{
-        // API CALL FOR ANTICIPOS LIST
+        getUnidades()
     },[])
 
-    const[rows, setRows] =useState([[1,2,3]])
+    const[unidades, setUnidades] =useState([])
     
-    const headings =["No. de Unidad", "Motor", "Placas"]
+    const headings =["No. de Unidad", "Modelo", "Motor", "Placas"]
     
+    async function getUnidades(){
+        const response = await fetch(`${localStorage.getItem('server-url')}/unidad/list`, {
+            headers : {
+                    'auth-token': localStorage.getItem('auth-token'),
+                } 
+            })
+        setUnidades(await response.json())
+    }
+
+    function newUnidad(){
+        ipcRenderer.sendSync('create-window',
+            ({
+                width:400, 
+                height:700, 
+                url: `${process.env.REACT_APP_URL}/unidad?mode=new`
+            })
+        )
+    }
+    
+    function openUnidad(clave){
+        ipcRenderer.sendSync('create-window',
+            ({
+                width:400, 
+                height:700, 
+                url: `${process.env.REACT_APP_URL}/unidad?clave=${clave}&mode=edit`
+            })
+        )
+    }
+
+    async function deleteUnidad(clave){
+        const result = ipcRenderer.sendSync('confirm', `¿Desea eliminar la unidad ${clave}?`)
+        
+        if (!result) return
+
+        const response = await fetch(`${localStorage.getItem('server-url')}/unidad/delete`,{
+            method: 'DELETE',
+            headers : {
+                    'auth-token': localStorage.getItem('auth-token'),
+                    'Content-Type': 'application/json',
+                },
+            body: JSON.stringify({clave})
+        })
+        
+        ipcRenderer.sendSync('alert', await response.json())
+        getUnidades()
+    }
+
     return(
         <>
             <h1 className="title">Unidades</h1>
+            <SearchBar filters={headings} function={null}/>
             <div className="frame">
-                <ul className="table-header">
-                    <li></li>
-                    {headings.map(heading=>{
-                        return(
-                            <li>{heading}</li>
-                        )
-                    })}
-                </ul>
-                {rows.map(row=>{
-                    return(
-                        <ul>
-                            <li className="pointer">Ver / Editar</li>
-                            {row.map(column=>{
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            {headings.map(heading=>{
                                 return(
-                                    <li>{column}</li>
+                                    <th key={heading}>{heading}</th>
                                 )
                             })}
-                        </ul>
-                    )
-                })}
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {unidades.map(unidad=>{
+                            return(
+                                <tr key={unidad.clave} className="row">
+                                    <td className="pointer" onClick={()=> openUnidad(unidad.clave)}>Abrir</td>
+                                    <td>{unidad.clave}</td>
+                                    <td>{unidad.modelo}</td>
+                                    <td>{unidad.motor}</td>
+                                    <td>{unidad.placas}</td>
+                                    <td onClick={()=> deleteUnidad(unidad.clave)} className="pointer delete">Borrar</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
             </div>
-            <br/>
-           <div className="button new ">
+            <div onClick={()=> newUnidad()} className="button new ">
                 <img src={Nuevo} alt=""/>
                 <span>Nueva Unidad</span>
-           </div>
+            </div>
         </>
     )
 }
